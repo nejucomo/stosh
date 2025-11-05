6b435ecf70fb58c81a538a06e56bf820f609c7e0
nobody <nobody@nowhere.net>
2025-11-05 14:40:02 -0800

    `#![deny(unsafe_code, missing_docs)]`

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! `partish` is a library and associated binary that runs an interactive "command-multiplexed" shell
+//!
+//! The term "command-multiplexed" means each command run in the user's shell has its own interactive ui widgets, which makes things like comparing two command runs or scrolling/collapsing the output of commands conveniently dynamic.
+//!
+//! This is different from a "terminal multiplexer" because there is only a single shell, and in fact, the author uses `partish` inside of a terminal multiplexer.
+#![deny(unsafe_code, missing_docs)]
 mod run;
 
 pub use self::run::run;
--- src/run.rs
+++ src/run.rs
@@ -1,3 +1,4 @@
+/// Run the full interactive app, using the process arguments
 pub async fn run() -> std::io::Result<()> {
     todo!("fixme")
 }
