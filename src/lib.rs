d36a394707b1f3bdd1fd565e7ab3679bc02af1c0
nobody <nobody@nowhere.net>
2025-11-05 14:34:32 -0800

    Basic binary skeleton.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod run;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use self::run::run;
